cdb806bbc55eec01c609402ade88aeb4335ade4a
David Busse <dpb@essub.com>
2012-07-01 22:56:57 -0500

    add named route for /contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Spree::Core::Engine.routes.prepend do
   resources :contact_messages
-  match '/contact' => 'contact_messages#new'
+  match '/contact' => 'contact_messages#new', :as => 'contact'
 end
